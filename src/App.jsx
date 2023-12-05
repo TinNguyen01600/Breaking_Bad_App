a37dff1b096aae731c4558f5a42c94a060484772
TinNguyen <ductin16111@gmail.com>
2023-12-05 03:48:46 +0200

    change to Ghibli_Studio_App because Breaking Bad API is not available

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import "./index.css"
+import axios from 'axios'
+import Header from './components/UI/Header'
 
 function App() {
-  return (
-    <>
-      Hello
-    </>
-  )
+    const [items, setItems] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchItems = async () => {
+            const res = await axios(`https://ghibliapi.vercel.app/films`)
+            console.log(res.data)
+            setItems(res.data)
+            setIsLoading(false)
+        }
+        fetchItems()
+    }, [])
+
+    return (
+        <>
+            <Header />
+        </>
+    )
 }
 
 export default App
